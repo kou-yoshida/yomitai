3aca6a0edd940c373b7a7dd7189f65739e3b0e02
kou-yoshida <kouyoshida28@gmail.com>
2024-03-09 15:33:46 +0900

    💚 CI通るように修正

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/src/auth";
 
-export async function GET(req: NextRequest) {
+export function GET(req: NextRequest) {
   const message = req.nextUrl.searchParams.get("message") || "null!!";
-
-  const session = await auth();
 
   return NextResponse.json(new Date() + message);
 }
